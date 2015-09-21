c004bfee94e51a43c3f06864846388e16bb54742
xhs <chef@dark.kitchen>
2015-09-21 18:11:49 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Git Prompt
+
+## Preview
+
+![Git Prompt Preview](https://raw.github.com/xhs/git-prompt/master/preview.png)
+
+## Installation
+
+```bash
+mkdir -vp ~/.bash
+cd ~/.bash
+git clone git://github.com/xhs/git-prompt.git
+```
+
+Edit your `~/.bash_profile` or `~/.profile` and add the following:
+
+```bash
+source ~/.bash/git-prompt/git-prompt.sh
+export PS1="\u@\H: \W \$git_branch\$git_dirty > "
+```
+
+## License
+
+[BSD 2-Clause](https://opensource.org/licenses/BSD-2-Clause)
---
+++ git-prompt.sh
@@ -0,0 +1,22 @@
+get_git_branch() {
+    local branch
+    if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+      if [[ "$branch" == "HEAD" ]]; then
+        branch='detached*'
+      fi
+      git_branch="($branch)"
+    else
+      git_branch="(none)"
+    fi
+}
+
+get_git_dirty() {
+    local status=$(git status --porcelain 2> /dev/null)
+    if [[ "$status" != "" ]]; then
+      git_dirty="*"
+    else
+      git_dirty=""
+    fi
+}
+
+PROMPT_COMMAND="get_git_branch; get_git_dirty; $PROMPT_COMMAND"
